36d3b455b80c084a62b061d8de5047880659fb11
Gabriel Siqueira <gabrielsiqueira.gsm@gmail.com>
2024-06-14 12:20:01 -0300

    feat: implementing the firsts templates for webserver using html

--- index.js
+++ index.js
@@ -28,6 +28,21 @@ const url = require('url');
 // console.log("Will read file!");
 
 ////////////////////////////////////// SERVER //////////////////////////////////////
+const replaceTemplate = (temp, movie) => {
+    let output = temp.replace(/{%MOVIENAME%}/g, movie.movieName);
+    output = output.replace(/{%STREAMING%}/g, movie.streaming);
+    output = output.replace(/{%GENRE%}/g, movie.mainGenre);
+    output = output.replace(/{%IMDB%}/g, movie.imdb);
+    output = output.replace(/{%YEAR%}/g, movie.year);
+    output = output.replace(/{%DESCRIPTION%}/g, movie.description);
+    output = output.replace(/{%ID%}/g, movie.id);
+
+    return output;
+}
+
+const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
+const tempMovie = fs.readFileSync(`${__dirname}/templates/template-movie.html`, 'utf-8');
+const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'utf-8');
 
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
@@ -35,15 +50,25 @@ const dataObj = JSON.parse(data);
 const server = http.createServer((req, res) => {
     const pathName = req.url;
 
-    if (pathName === '/') {
-        res.end('There is nothing to see here');
-    } else if (pathName === '/overview') {
-        res.end('This is the overview!');
+    // Overview page
+    if (pathName === '/' || pathName === '/overview') {
+        res.writeHead(200, {'Content-type': 'text/html'});
+
+        const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
+        const output = tempOverview.replace('{%MOVIE_CARDS%}', cardsHtml)
+        res.end(output);
+
+    // Movies page
     } else if (pathName === '/movies') {
-        res.end('This is the movies page!');
+        res.writeHead(200, {'Content-type': 'text/html'});
+        res.end(tempMovie);
+    
+    // API
     } else if (pathName === '/api') {
         res.writeHead(200, {'Content-type': 'application/json'});
         res.end(data);
+    
+    // Not found
     } else {
         res.writeHead(404, {
             'Content-Type': 'text/html',
