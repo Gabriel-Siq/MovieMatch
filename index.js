fe5768a17584b2a1067c2be75f793ec3411cd26b
Gabriel Siqueira <gabrielsiqueira.gsm@gmail.com>
2024-07-01 17:02:59 -0300

    fex: change port

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const fs = require('fs');
 const http = require('http');
 const url = require('url');
-const replaceTemplate = require('./modules/replaceTemplate')
+const replaceTemplate = require('./modules/replaceTemplate');
 
 const tempOverview = fs.readFileSync(`${__dirname}/templates/template-overview.html`, 'utf-8');
 const tempMovie = fs.readFileSync(`${__dirname}/templates/template-movie.html`, 'utf-8');
@@ -10,30 +10,33 @@ const tempCard = fs.readFileSync(`${__dirname}/templates/template-card.html`, 'u
 const data = fs.readFileSync(`${__dirname}/dev-data/data.json`, 'utf-8');
 const dataObj = JSON.parse(data);
 
+const port = process.env.PORT || 8000;
+const hostname = '0.0.0.0';
+
 const server = http.createServer((req, res) => {
     const { query, pathname } = url.parse(req.url, true);
 
     // Overview page
     if (pathname === '/' || pathname === '/overview') {
-        res.writeHead(200, {'Content-type': 'text/html'});
+        res.writeHead(200, { 'Content-type': 'text/html' });
 
         const cardsHtml = dataObj.map(el => replaceTemplate(tempCard, el)).join('');
-        const output = tempOverview.replace('{%MOVIE_CARDS%}', cardsHtml)
+        const output = tempOverview.replace('{%MOVIE_CARDS%}', cardsHtml);
         res.end(output);
 
     // Movies page
     } else if (pathname === '/movies') {
-        res.writeHead(200, {'Content-type': 'text/html'});
+        res.writeHead(200, { 'Content-type': 'text/html' });
         
         const movie = dataObj[query.id];
         const output = replaceTemplate(tempMovie, movie);
         res.end(output);
-    
+
     // API
     } else if (pathname === '/api') {
-        res.writeHead(200, {'Content-type': 'application/json'});
+        res.writeHead(200, { 'Content-type': 'application/json' });
         res.end(data);
-    
+
     // Not found
     } else {
         res.writeHead(404, {
@@ -44,6 +47,6 @@ const server = http.createServer((req, res) => {
     }
 });
 
-server.listen(8000, 'localhost', () => {
-    console.log("Server has been started on port 8000!");
+server.listen(port, hostname, () => {
+    console.log(`Server running at http://${hostname}:${port}/`);
 });
